f5ae7ed2959823b54f09f861e489684058d4d2d5
aapozd <aapozdnyakove@gmail.com>
2022-05-23 15:55:23 +0300

    [CTRL] Dirty context for WardDataBaseControllerTest

--- src/test/java/WardDataBaseControllerTest.java
+++ src/test/java/WardDataBaseControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class WardDataBaseControllerTest {
     @Autowired
     MockMvc mockMvc;
