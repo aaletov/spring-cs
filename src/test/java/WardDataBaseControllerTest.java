024b74f7ad0d7832cf55544c4f945c12548f2be9
aapozd <aapozdnyakove@gmail.com>
2022-05-23 05:05:01 +0300

    [TEST] Cover WardDataBaseController GET, POST methods

---
+++ src/test/java/WardDataBaseControllerTest.java
@@ -0,0 +1,56 @@
+import cs.Main;
+import cs.repos.WardRepository;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+public class WardDataBaseControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    WardRepository wardRepository;
+
+    @Test
+    public void testGetWardById() throws Exception {
+        assertTrue(wardRepository.findWardById(1).isPresent());
+
+        mockMvc.perform(get("/api/ward/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        Map<String, String> bodyMap = new HashMap<>() {{
+            put("peopleList", "[]");
+            put("name", "Ward 10");
+        }};
+
+        JSONObject bodyJSON = new JSONObject(bodyMap);
+
+        mockMvc.perform(post("/api/ward/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(bodyJSON.toString()))
+                .andExpect(status().isOk());
+    }
+
+}
