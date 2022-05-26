5849d94469b47d4ac24c902ae33060dadf5d78de
aapozd <aapozdnyakove@gmail.com>
2022-05-26 03:36:17 +0300

    [FRONT] Fix routes for home and /

--- src/main/java/cs/views/HomeView.java
+++ src/main/java/cs/views/HomeView.java
@@ -4,7 +4,7 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("")
+@Route("home")
 public class HomeView extends VerticalLayout {
     HomeView() {
         add(new Text("Welcome to homepage"));
--- src/main/java/cs/views/MainView.java
+++ src/main/java/cs/views/MainView.java
@@ -4,7 +4,7 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("home")
+@Route("")
 public class MainView extends VerticalLayout {
 
     public MainView() {
